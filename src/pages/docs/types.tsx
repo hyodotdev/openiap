d8db1f395f028ee284a6b9dea2a027786717f71f
hyochan <dooboolab@gmail.com>
2025-09-17 08:56:56 +0900

    chore: link openiap-gql v1.0.2 downloads

--- src/pages/docs/types.tsx
+++ src/pages/docs/types.tsx
@@ -5,11 +5,11 @@ import CodeBlock from '../../components/CodeBlock';
 import PlatformTabs from '../../components/PlatformTabs';
 import { useScrollToHash } from '../../hooks/useScrollToHash';
 
-const RELEASE_VERSION = '1.0.1';
+const RELEASE_VERSION = '1.0.2';
 const RELEASE_PAGE_URL =
-  'https://github.com/hyodotdev/openiap-gql/releases/tag/1.0.1';
+  'https://github.com/hyodotdev/openiap-gql/releases/tag/1.0.2';
 const RELEASE_DOWNLOAD_PREFIX =
-  'https://github.com/hyodotdev/openiap-gql/releases/download/1.0.1/';
+  'https://github.com/hyodotdev/openiap-gql/releases/download/1.0.2/';
 
 const SELECT_CARET_ICON =
   'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2212%22%20height%3D%228%22%20viewBox%3D%220%200%2012%208%22%20fill%3D%22none%22%3E%3Cpath%20d%3D%22M1%201l5%205%205-5%22%20stroke%3D%22%23a2a9b0%22%20stroke-width%3D%221.5%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22/%3E%3C/svg%3E';
