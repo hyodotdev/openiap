653fff0a6973c37ab80dcb355319c26fff8dec48
hyochan <dooboolab@gmail.com>
2025-10-20 01:54:36 +0900

    fix: ci

--- packages/google/openiap/src/test/java/dev/hyo/openiap/OpenIapErrorTest.kt
+++ packages/google/openiap/src/test/java/dev/hyo/openiap/OpenIapErrorTest.kt
@@ -159,7 +159,7 @@ class OpenIapErrorTest {
     fun `ServiceUnavailable has correct code and message`() {
         val error = OpenIapError.ServiceUnavailable
         assertEquals(ErrorCode.ServiceError.rawValue, error.code)
-        assertEquals("Google Play service is unavailable", error.message)
+        assertEquals("Billing service is unavailable", error.message)
     }
 
     @Test
@@ -270,34 +270,36 @@ class OpenIapErrorTest {
     fun `getAllErrorCodes returns all error codes and messages`() {
         val allCodes = OpenIapError.getAllErrorCodes()
 
-        // Check that all expected codes are present
+        // Check that all expected codes are present (using actual object CODEs)
         val expectedCodes = setOf(
-            ErrorCode.SkuNotFound.rawValue,
-            ErrorCode.PurchaseError.rawValue,
-            ErrorCode.UserCancelled.rawValue,
-            ErrorCode.DeferredPayment.rawValue,
-            ErrorCode.NetworkError.rawValue,
-            ErrorCode.Unknown.rawValue,
-            ErrorCode.NotPrepared.rawValue,
-            ErrorCode.InitConnection.rawValue,
-            ErrorCode.QueryProduct.rawValue,
-            ErrorCode.EmptySkuList.rawValue,
-            ErrorCode.SkuNotFound.rawValue,
-            ErrorCode.SkuOfferMismatch.rawValue,
-            ErrorCode.UserCancelled.rawValue,
-            ErrorCode.AlreadyOwned.rawValue,
-            ErrorCode.ItemNotOwned.rawValue,
-            ErrorCode.BillingUnavailable.rawValue,
-            ErrorCode.ItemUnavailable.rawValue,
-            ErrorCode.DeveloperError.rawValue,
-            ErrorCode.FeatureNotSupported.rawValue,
-            ErrorCode.ServiceDisconnected.rawValue,
-            ErrorCode.UserError.rawValue,
-            ErrorCode.ServiceError.rawValue,
-            ErrorCode.ReceiptFailed.rawValue,
-            ErrorCode.TransactionValidationFailed.rawValue,
-            ErrorCode.SyncError.rawValue,
-            ErrorCode.ActivityUnavailable.rawValue
+            OpenIapError.ProductNotFound.CODE,
+            OpenIapError.PurchaseFailed.CODE,
+            OpenIapError.PurchaseCancelled.CODE,
+            OpenIapError.PurchaseDeferred.CODE,
+            OpenIapError.NetworkError.CODE,
+            OpenIapError.UnknownError.CODE,
+            OpenIapError.NotPrepared.CODE,
+            OpenIapError.InitConnection.CODE,
+            OpenIapError.QueryProduct.CODE,
+            OpenIapError.EmptySkuList.CODE,
+            OpenIapError.SkuNotFound.CODE,
+            OpenIapError.SkuOfferMismatch.CODE,
+            OpenIapError.UserCancelled.CODE,
+            OpenIapError.ItemAlreadyOwned.CODE,
+            OpenIapError.ItemNotOwned.CODE,
+            OpenIapError.ServiceUnavailable.CODE,
+            OpenIapError.BillingUnavailable.CODE,
+            OpenIapError.ItemUnavailable.CODE,
+            OpenIapError.DeveloperError.CODE,
+            OpenIapError.FeatureNotSupported.CODE,
+            OpenIapError.ServiceDisconnected.CODE,
+            OpenIapError.ServiceTimeout.CODE,
+            OpenIapError.PaymentNotAllowed.CODE,
+            OpenIapError.BillingError.CODE,
+            OpenIapError.InvalidReceipt.CODE,
+            OpenIapError.VerificationFailed.CODE,
+            OpenIapError.RestoreFailed.CODE,
+            OpenIapError.MissingCurrentActivity.CODE
         )
 
         assertEquals(expectedCodes.size, allCodes.size)
